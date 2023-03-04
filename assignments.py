cb507a1547562c42c9ce31628ac63c035c23fd8a
Ayo <olubayo.ayobami21@gmail.com>
2023-03-04 23:49:01 +0000

    ini

---
+++ assignments.py
@@ -0,0 +1,10 @@
+#A test to check input of string knowledge 
+name = input("Enter programmer's name:")
+print('Hello and welcome genius:', name)
+print("My name is Ayo et i'm learning Python")
+
+#A little calculator
+hours = input('Enter hours:')
+rate = input('Enter rate:')
+Pay = float(hours) * float(rate)
+print('Pay:', Pay)
